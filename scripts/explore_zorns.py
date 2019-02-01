46ce8fee47ed74fda01e9f70d79d0694710fc2ac
nanma80 <mananself@gmail.com>
2019-02-01 00:19:26 -0800

    mix3

--- scripts/explore_zorns.py
+++ scripts/explore_zorns.py
@@ -32,8 +32,12 @@ h = Zorn([[1, [0, 1, 0]], [[1, 0, 1], 1]])
 
 # print 'jh:', (j * h)
 
-mix1 = (i * j) * h # (12)(34) -> 1(23)4 swap middle => 2413
-mix2 = i * (j * h) # 2134 (12)34 -> 1(234) => ??? 2413*
+mix1 = (i * j) * h # (12)(34) -> 1(23)4 swap middle => 2413. orbit3
+mix2 = i * (j * h) # 2134 (12)34 -> 1(234) => ??? 2413*. orbit 4
+mix3 = i * (h * j) # 2134 (12)34 -> 1(234) => ??? 2341*. orbit 0
+mix2inv = (h * i) * j # 3142. orbit 3
+mix3inv = (j * h) * i # 4123. orbit 3
+
 print mix1 # orbit 3
 print mix1 * mix1 # orbit 4
 print mix1 * mix1 * mix1 # mix1^3 = 1 not ^4; orbit 3
@@ -44,14 +48,25 @@ print mix2 * mix2 # orbit 3. What permutation is it? inv(mix2) == (h * i) * j
 print mix2 * mix2 * mix2 # mix2^3 = 1 not ^4; orbit 3
 print
 
-print mix2 * mix2 == (h * i) * j # 1324 -> 3124 -> 3142
-print (h * i) * j * mix2 # 1
+print mix2 * mix2 == mix2inv # 1324 -> 3124 -> 3142
+print mix2inv * mix2 # 1
+print mix2 * mix2inv # 1
 print
 
-print j * h
-print (j * h) * (j * h)
-print j * h * j * h # same as above. Moufang loop: two generators: always associative
-print (j * h) * (j * h) * (j * h)
-print j * h * j * h * j * h # same as above. Moufang loop: two generators: always associative
+print mix3 # orbit 0
+print mix3 * mix3 # orbit 3. What permutation is it? inv(mix3) == ?
+print mix3 * mix3 * mix3 # mix2^3 = 1 not ^4; orbit 3
+print
+
+print mix3 * mix3 == mix3inv
+print mix3inv * mix3
+print mix3 * mix3inv
+print
+
+# print j * h
+# print (j * h) * (j * h)
+# print j * h * j * h # same as above. Moufang loop: two generators: always associative
+# print (j * h) * (j * h) * (j * h)
+# print j * h * j * h * j * h # same as above. Moufang loop: two generators: always associative
 
 # print i * j * h * i * j * h * j * h * i * j * h * j # identity
